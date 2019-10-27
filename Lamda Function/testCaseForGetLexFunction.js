709e30de315386c0edc1b629e2039cf0b05099b6
alanapte <aptealan@gmail.com>
2019-10-26 20:01:39 -0700

    Add files via upload

---
+++ Lamda Function/testCaseForGetLexFunction.js
@@ -0,0 +1,21 @@
+{
+  "messageVersion": "1.0",
+  "invocationSource": "FulfillmentCodeHook",
+  "userId": "user-1",
+  "sessionAttributes": {},
+  "bot": {
+    "name": "ReminderApp",
+    "alias": "$LATEST",
+    "version": "$LATEST"
+  },
+  "outputDialogMode": "Text",
+  "currentIntent": {
+    "name": "OrderPizza",
+    "slots": {
+      "RemindBody": "Laundry",
+      "RemindDays": "Sunday",
+      "RemindTime": "4 p.m"
+    },
+    "confirmationStatus": "None"
+  }
+}
