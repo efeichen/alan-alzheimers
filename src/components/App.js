8a9f58c20465c12eece76ed31cbdf009e1e12253
Kenneth Chen <efeichen@gmail.com>
2019-10-26 17:17:20 -0700

    More configs

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Amplify, { Interactions } from 'aws-amplify';
+import { ChatBot, AmplifyTheme } from 'aws-amplify-react';
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
+
+// Imported default theme can be customized by overloading attributes
+const myTheme = {
+    ...AmplifyTheme,
+    sectionHeader: {
+        ...AmplifyTheme.sectionHeader,
+        backgroundColor: '#ff6600'
+    }
+};
+
+class App extends Component {
+
+    handleComplete(err, confirmation) {
+        if (err) {
+            alert('Bot conversation failed')
+            return;
+        }
+
+        alert('Success: ' + JSON.stringify(confirmation, null, 2));
+        return 'Trip booked. Thank you! what would you like to do next?';
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <ChatBot
+                    title="My Bot"
+                    theme={myTheme}
+                    botName="alan_master"
+                    welcomeMessage="Welcome, how can I help you today?"
+                    onComplete={this.handleComplete.bind(this)}
+                    clearOnComplete={true}
+                    conversationModeOn={false}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
